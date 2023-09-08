f771c3394627ec0527797483f882d3c777809564
Masterain <i@irain.in>
2023-09-07 17:05:51 -0700

    create dgp tools

---
+++ run.sh
@@ -0,0 +1,18 @@
+# Docker Image Settings
+imageName=dgp-github-bot
+containerName=DGP-GitHub-Bot
+imageVersion=1.9
+externalPort=3524
+internalPort=8080
+
+oldContainer=`docker ps -a| grep ${containerName} | head -1|awk '{print $1}' `
+echo Delete old container...
+docker rm  $oldContainer -f
+echo Delete success
+
+docker build -f Dockerfile -t $imageName:$imageVersion .
+docker run -d -itp $externalPort:$internalPort \
+    -v $(pwd)/.env:/app/.env \
+    --restart=always \
+    --name="$containerName" \
+    $imageName:$imageVersion
