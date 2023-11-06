a0b5e63c9ad3ce20314de066723ee334c2714906
Masterain <i@irain.in>
2023-11-06 02:59:28 -0800

    Update push.py

--- push.py
+++ push.py
@@ -17,17 +17,29 @@ async def find_fixed_issue(repo_name: str, commit_id: str, message: str) -> str:
             print(f"Commit {commit_id} fixed issue {issue_number}")
             issue_node_id = get_issue_node_id(repo_name, issue_number)
             current_issue_labels = get_issue_label(repo_name, issue_number)
-            if "已修复" in current_issue_labels:
+            if "已修复" in current_issue_labels or "已完成" in current_issue_labels:
                 print(f"Issue {issue_number} already fixed, skip")
                 continue
+            is_bug = True if "BUG" in current_issue_labels else False
+            is_feat = True if "Feat" in current_issue_labels else False
             issue_language = get_issue_language(repo_name, issue_number)
             if issue_language == "CHS":
-                return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} 已修复此问题")
+                if is_bug:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} 已修复此问题")
+                elif is_feat:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} 已实现此议题")
+                else:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} 已完成此议题")
             elif issue_language == "ENG":
-                return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} fixed this issue")
+                if is_bug:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} fixed this bug")
+                elif is_feat:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} implemented this feature")
+                else:
+                    return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} finished this issue")
             else:
-                pass
-            return_result += add_issue_label(repo_name, issue_number, ["已修复", "等待发布"])
+                return_result += make_issue_comment(repo_name, issue_number, f"{commit_id} finished this issue")
+            return_result += add_issue_label(repo_name, issue_number, ["已完成", "等待发布"])
             return_result += add_issue_to_project_board_with_number_and_column_name(org_name=org_name,
                                                                                     issue_node_id=issue_node_id,
                                                                                     project_number=2,
