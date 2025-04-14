9575aedf49b47d895ae95a30b759471a0370247c
Masterain <i@irain.in>
2025-04-13 21:44:32 -0700

    add sentry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+import sentry_sdk
 from fastapi import FastAPI, Request
 from issue import issue_handler
 from issue_comment import comment_handler
@@ -6,6 +7,36 @@ from pull_request import pull_request_handler
 from push import push_handler
 from release import release_handler
 import admin
+from config import SENTRY_TOKEN_URL
+from sentry_sdk.integrations.starlette import StarletteIntegration
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+
+
+def before_send(event, hint):
+    request = event.get("request", {})
+    headers = request.get("headers", {})
+    github_delivery = headers.get("X-GitHub-Delivery")
+    if github_delivery:
+        event.setdefault("tags", {})["github_delivery"] = github_delivery
+    return event
+
+
+sentry_sdk.init(
+    dsn=f"https://{SENTRY_TOKEN_URL}",
+    send_default_pii=True,
+    integrations=[
+        StarletteIntegration(
+            transaction_style="url",
+            failed_request_status_codes={403, *range(500, 599)},
+        ),
+        FastApiIntegration(
+            transaction_style="url",
+            failed_request_status_codes={403, *range(500, 599)},
+        ),
+    ],
+    profiles_sample_rate=1.0,
+    before_send=before_send
+)
 
 app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(admin.router)
@@ -18,8 +49,9 @@ async def root():
 
 @app.post("/payload")
 async def payload(request: Request):
+    headers = request.headers
     result = "Something went wrong"
-    hook_type = request.headers.get("X-GitHub-Event")
+    hook_type = headers.get("X-GitHub-Event")
     request_payload = await request.json()
     print(f"Receiving: Hook type: {hook_type}")
 
